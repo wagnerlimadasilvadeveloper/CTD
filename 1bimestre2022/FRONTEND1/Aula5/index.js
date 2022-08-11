41828b3d35337dcea469d809b8424937e822143d
Wagner <wagnerlima.coach@gmail.com>
2022-08-10 21:04:45 -0300

    lista_de_urgências

---
+++ 1bimestre2022/FRONTEND1/Aula5/index.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Lista de Urgências</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <p>Integrantes do Grupo</p>
+        
+    <h1 class="cor_de_destaque">
+
+    </h1>
+    </body>
+</html>
